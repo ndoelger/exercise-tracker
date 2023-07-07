6b91e40aa2d0279081d6851c55286e255a8aea82
ndoelger <doelgern@gmail.com>
2023-07-07 15:01:16 -0400

    Basic CRUD

--- main_app/models.py
+++ main_app/models.py
@@ -3,14 +3,14 @@ from django.urls import reverse
  
 # Create your models here.
 class Exercise(models.Model):
-    name: models.CharField(max_length=100)
-    musclegroups: models.CharField(max_length=100)
-    intensity: models.IntegerField()
-    directions: models.TextField(max_length=500)
-    completed: models.BooleanField(default=False)
+    name = models.CharField(max_length=100, default ='Exercise')
+    musclegroups = models.CharField(max_length=100, default ='Musclegroup')
+    intensity = models.IntegerField(default=0)
+    directions = models.TextField(max_length=500, default='direction')
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'wine_id': self.id})
+        return reverse('exercise_details', kwargs={'pk': self.id})
